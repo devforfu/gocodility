b834d4f7e63ec6bac365f621b17348a1c9562974
devforfu <developer.z@outlook.com>
2018-12-13 16:04:53 +0500

    Prefix sums of counters

--- src/dna/dna.go
+++ src/dna/dna.go
@@ -1,6 +1,7 @@
 package dna
 
-var impactFactor = map[int32]int {'C': 1, 'D': 2}
+var impactFactor = map[int32]int {'A': 1, 'C': 2, 'G': 3, 'T': 4}
+
 
 // Find the minimal nucleotide from a range of sequence DNA
 func QueryGenomicRange(s string, p []int, q []int) []int {
@@ -9,14 +10,36 @@ func QueryGenomicRange(s string, p []int, q []int) []int {
 
 // Counts each letter presence for each prefix of string s
 func countLetters(s string) [][]int {
-    prev := make([]int, 4)
-    next := make([]int, 4)
-    counters := [][]int { prev }
+    prev := empty()
+    counters := [][]int { empty() }
     for _, char := range s {
+        next := empty()
         copy(next, prev)
-        next[impactFactor[char]] += 1
+        next[impactFactor[char] - 1] += 1
         counters = append(counters, next)
         copy(prev, next)
     }
     return counters
+}
+
+func empty() []int {
+    return make([]int, 4)
+}
+
+func computeImpact(s string, counts [][]int, p int, q int) int {
+    if p == q { return impactFactor[int32(s[p])] }
+    a, b := counts[p], counts[q + 1]
+    diff := diffCounts(a, b)
+    for i, x := range diff {
+        if x > 0 { return i + 1 }
+    }
+    panic("cannot get there!")
+}
+
+func diffCounts(a []int, b []int) []int {
+    diff := make([]int, 4)
+    for i := 0; i < 4; i++ {
+        diff[i] = b[i] - a[i]
+    }
+    return diff
 }
